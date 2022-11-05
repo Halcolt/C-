35c7ba8ce0cf0e0014a5eff073bc52a448835d1f
Halcolt <billtrinh19@gmail.com>
2022-11-05 22:31:14 +0700

    update

--- Week2/Ex2.5.c
+++ Week2/Ex2.5.c
@@ -1,31 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
 typedef struct pbook
 {
-    char name[100];
-    int phonenum;
-    char email[100];
-    char address[100];
+    char name[30];
+    char phonenum[15];
+    char email[30];
+    char address[30];
 } pbook_t;
 
 int main()
 {
+    FILE *p;
     pbook_t info[100];
     p = fopen("Phonebook.txt","w+");
     printf("Nhap so luong thong tin nguoi dung: ");
     int a;
-    scanf("%s",&a);
+    scanf("%d",&a);
+    printf("----------------------------------\n");
     for (int i=0; i<a; i++)
     {
-        printf("Nhap ten nguoi thu %d: ",i+1);
-        scanf("%c", info->name);
+        printf("\nNhap ten nguoi thu %d: ",i+1);
+        scanf("%s", &info[i].name);
         printf("Nhap SDT nguoi thu %d: ",i+1);
-        scanf("%d", info->phonenum);
+        scanf("%s", &info[i].phonenum);
         printf("Nhap email nguoi thu %d: ",i+1);
-        scanf("%c", info->email);
+        scanf("%s", &info[i].email);
         printf("Nhap dia chi nguoi thu %d: ",i+1);
-        scanf("%c", info->address);
+        scanf("%s \n", &info[i].address);
         printf("----------------------------------\n");
     }
-    fwrite()
-    fclose(p)
+
+    fwrite(info, a, sizeof(pbook_t), p);
+    fclose(p);
 }
